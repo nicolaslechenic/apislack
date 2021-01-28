6fa7097f085f62ef644370ef54fa8c0cec44dd31
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2021-01-28 06:54:01 +0100

    yaaa

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
-$res = array('token' => 'xoxb-1652175914689-1652276326993-n9cGyTtmZWnOly6h78XDQh9H', 'remote' => $_SERVER['HTTP_REFERER']);
 
-echo(json_encode($res));
+if($_SERVER['HTTP_REFERER'] == "https://elastic-babbage-fbe20d.netlif.app/") {
+  $res = array('token' => 'xoxb-1652175914689-1652276326993-n9cGyTtmZWnOly6h78XDQh9H', 'remote' => $_SERVER['HTTP_REFERER']);
+
+  echo(json_encode($res));
+} else {
+  http_response_code(401);
+}
+
