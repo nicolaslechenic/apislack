7af18ba430f125f663b0c05d24e98ccc8fbf766d
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2021-01-28 06:48:41 +0100

    ya

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-$res = array('token' => 'xoxb-1652175914689-1652276326993-n9cGyTtmZWnOly6h78XDQh9H', 'remote' => $_SERVER['REMOTE_HOST']);
+$res = array('token' => 'xoxb-1652175914689-1652276326993-n9cGyTtmZWnOly6h78XDQh9H', 'remote' => $_SERVER['HTTP_REFERER']);
 
 echo(json_encode($res));
